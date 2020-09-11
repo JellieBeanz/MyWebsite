96648b13a1408157ba991ee4fbd9928ed566aeec
JellieBeanz <33373091+JellieBeanz@users.noreply.github.com>
2020-09-11 20:50:04 +0100

    added temp recipies to local storage

--- static/js/RAppscript.js
+++ static/js/RAppscript.js
@@ -8,6 +8,10 @@ const mealPopup = document.getElementById('meal-popup');
 const mealInfoEl = document.getElementById('meal-info');
 const popupCloseBtn = document.getElementById('close-popup');
 
+const tempFavs = ["52837", "52889", "52864", "52897", "52956"]
+
+localStorage.setItem('mealIds', JSON.stringify(tempFavs) )
+
 getRandomMeal();
 
 fetchFavorites()
